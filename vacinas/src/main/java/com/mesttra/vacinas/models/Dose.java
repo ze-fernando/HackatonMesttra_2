78bb553e1819dc2411fb2b219d2fee7991313031
Miriã <miria5389@gmail.com>
2025-02-15 22:33:28 -0300

    Models
    models

--- vacinas/src/main/java/com/mesttra/vacinas/models/Dose.java
+++ vacinas/src/main/java/com/mesttra/vacinas/models/Dose.java
@@ -2,5 +2,47 @@ package com.mesttra.vacinas.models;
 
 public class Dose {
 
-    
+    private int id;
+    private int id_vacina;
+    private String dose;
+    private int idade_recomendada_aplicacao;
+
+    public Dose(int id, int id_vacina, String dose, int idade_recomendada_aplicacao){
+        this.id = id;
+        this.id_vacina = id_vacina;
+        this.dose = dose;
+        this.idade_recomendada_aplicacao = idade_recomendada_aplicacao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId_vacina() {
+        return id_vacina;
+    }
+
+    public void setId_vacina(int id_vacina) {
+        this.id_vacina = id_vacina;
+    }
+
+    public String getDose() {
+        return dose;
+    }
+
+    public void setDose(String dose) {
+        this.dose = dose;
+    }
+
+    public int getIdade_recomendada_aplicacao() {
+        return idade_recomendada_aplicacao;
+    }
+
+    public void setIdade_recomendada_aplicacao(int idade_recomendada_aplicacao) {
+        this.idade_recomendada_aplicacao = idade_recomendada_aplicacao;
+    }
 }
--- vacinas/src/main/java/com/mesttra/vacinas/models/Imunizacoes.java
+++ vacinas/src/main/java/com/mesttra/vacinas/models/Imunizacoes.java
@@ -1,6 +1,96 @@
 package com.mesttra.vacinas.models;
 
-public class Imunizacoes {
+import java.time.LocalDate;
 
-    
+public class Imunizacoes {
+    private int id;
+    private int id_paciente;
+    private int id_dose;
+    private LocalDate data_aplicacao;
+    private String fabricante;
+    private String lote;
+    private String local_aplicacao;
+    private String profissional_aplicador;
+
+    public Imunizacoes(int id, int id_paciente, int id_dose, LocalDate data_aplicacao, String fabricante, String lote, String local_aplicacao, String profissional_aplicador) {
+        this.id = id;
+        this.id_paciente = id_paciente;
+        this.id_dose = id_dose;
+        this.data_aplicacao = data_aplicacao;
+        this.fabricante = fabricante;
+        this.lote = lote;
+        this.local_aplicacao = local_aplicacao;
+        this.profissional_aplicador = profissional_aplicador;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    // -----
+
+    public int getId_paciente(){
+        return id_paciente;
+    }
+
+    public void setId_paciente(){
+        this.id_paciente = id_paciente;
+    }
+    // ------
+
+    public int getId_dose(){
+        return id_dose;
+    }
+
+    public void setId_dose(){
+        this.id_dose = id_dose;
+    }
+    // -----
+
+    public LocalDate getData_aplicacao() {
+        return data_aplicacao;
+    }
+
+    public void setData_aplicacao(){
+        this.data_aplicacao = data_aplicacao;
+    }
+    // -----
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+    // ------
+
+    public String getLote() {
+        return lote;
+    }
+
+    public void setLote(String lote) {
+        this.lote = lote;
+    }
+    // ----
+
+    public String getLocal_aplicacao() {
+        return local_aplicacao;
+    }
+
+    public void setLocal_aplicacao(String local_aplicacao) {
+        this.local_aplicacao = local_aplicacao;
+    }
+    // -----
+
+    public String getProfissional_aplicador() {
+        return profissional_aplicador;
+    }
+
+    public void setProfissional_aplicador(String profissional_aplicador) {
+        this.profissional_aplicador = profissional_aplicador;
+    }
 }
